7a0e5a799ae8794befec792f86cb6e76fad60fb1
MurrayCoding <murraycoding@gmail.com>
2021-04-06 09:20:41 -0400

    added about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import SectionTitle from '../components/SectionTitle'
+
+export default function AboutPage() {
+  return (
+    <>
+      <p>This is the about page.</p>
+      <SectionTitle name="freelancer" />
+      <p>As a freelance web developer, I help small companies either create a new website or update old sites to be responsive and built with modern web tools.</p>
+    </>
+  )
+}
