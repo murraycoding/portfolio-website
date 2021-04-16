661a51b3d8f89f884133dcba737791a333cc8c06
MurrayCoding <murraycoding@gmail.com>
2021-04-16 16:29:51 -0400

    tested out the new project card component

--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,32 @@
 import SectionTitle from '../components/SectionTitle'
-import ProjectCard from '../components/ProjectCard'
+import NewProjectCard from '../components/NewProjectCard'
 
 export default function ProjectsPage() {
   return (
     <>
-      <SectionTitle name="projects" />
+      <SectionTitle name="projects"/>
+      <div className="cardHolder">
+        <NewProjectCard 
+        title="murray math tutoring"
+        description="Murray Math Tutoring is a static website built with NextJS which features a unique mobile navigation menu."
+        github="https://github.com/murraycoding/murray-math-tutoring"
+        link="https://www.murraymathtutoring.com" />
+        <NewProjectCard 
+        title="portfolio website"
+        description="This is a test of the new project card This is a test of the new project card This is a test of the new project card This is a test of the new project card This is a test of the new project card"
+        github="https://github.com/murraycoding/portfolio-website"
+        link="/" />
+        <NewProjectCard 
+        title="murray math tutoring"
+        description="This is a test of the new project card"
+        github="https://github.com/murraycoding/murray-math-tutoring"
+        link="https://www.murraymathtutoring.com" />
+        <NewProjectCard 
+        title="murray math tutoring"
+        description="This is a test of the new project card"
+        github="https://github.com/murraycoding/murray-math-tutoring"
+        link="https://www.murraymathtutoring.com" />
+      </div>
     </>
   )
 }
