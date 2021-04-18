fa12bb2eb5b870812a00ff3166eb49f952a293d4
MurrayCoding <murraycoding@gmail.com>
2021-04-18 00:46:10 -0400

    updated the contact form.

--- pages/contact.js
+++ pages/contact.js
@@ -6,10 +6,18 @@ export default function ContactPage() {
     <form id="contact-form" method="post">
       <SectionTitle name="Contact Info" />
       <label>Name</label>
-      <input placeholder="Type Name Here"></input>
+      <input placeholder="Type Name Here" />
       <label>Email</label>
-      <input type="email" placeholder="name@awesomecompany.com"></input>
+      <input type="email" placeholder="name@awesomecompany.com" />
+      <label>Phone</label>
+      <input placeholder="(555)867-5309"></input>
       <SectionTitle name="Message" />
+      <label>Reason for contact</label>
+      <select>
+        <option>Option 1</option>
+      </select>
+      <label>Message</label>
+      <textarea rows="4"/>
     </form>
   )
 }
