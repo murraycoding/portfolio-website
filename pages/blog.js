0bbfa6497288620874a9b5612ad4b18938fe31b1
MurrayCoding <murraycoding@gmail.com>
2021-04-22 22:01:39 -0400

    blog posts now all show on blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,14 +1,35 @@
 import BlogPreview from '../components/BlogPreview'
+import client from '../client'
+import BlockContent, { propTypes } from '@sanity/block-content-to-react'
 
-export default function BlogPage() {
+export default function BlogPage({ ...info }) {
+
+  // creates an array of posts to turn into components
+  const infoArray = Object.values(info)
+
+  const blogPreviews = infoArray.map(
+    (post) => {
+      console.log("This is here")
+      return <BlogPreview
+        title = {post.title}
+        date = {post._createdAt}
+        content = {post.body[0].children[0].text}
+      />
+    }
+  )
+
   return (
     <>
     <p>This is the blog page.</p>
-    <BlogPreview
-      title="Brian"
-      date="cheese"
-      content="This is the best content in the whole wide world."
-    />
+    {blogPreviews}
     </>
   )
+}
+
+BlogPage.getInitialProps = async function(context) {
+  //returns a list of all of the posts
+  const info = await client.fetch(`*[_type == "post"]`)
+
+  //returns the information to the component.
+  return info
 }
