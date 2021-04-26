ba45a3fb2a13bda19ae70366d60d61138adc6d63
MurrayCoding <murraycoding@gmail.com>
2021-04-25 23:00:36 -0400

    updated footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,23 @@
+import FooterStyles from '../styles/Footer.module.scss'
+import FooterContent from './FooterContent'
+
 export default function Footer(){
   return (
     <footer>
-      <p>This is the new footer.</p>
+      <div className="cardHolder">
+        <FooterContent title="Get In Touch">
+          Email: murraycoding@gmail.com Twitter: @murraycoding
+        </FooterContent>
+        <FooterContent title="Work with Me">
+          My freelance experience is tailored to help small businesses better their website to attract more customers.
+        </FooterContent>
+        <FooterContent title="Twitter">
+          This is the content.
+        </FooterContent>
+        <FooterContent title="My Latest Project">
+          This is the content.
+        </FooterContent>
+      </div>
     </footer>
   )
 }
